ccfcb9c619c30f3a3de443854e4e24e061ab0465
lisong <song.li@witontek.com>
2014-08-05 17:09:49 +0800

    add

---
+++ test-cxf/src/main/java/ls/demon/test_cxf/App.java
@@ -0,0 +1,13 @@
+package ls.demon.test_cxf;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
