037641bb7866c566f9e0fb7dc8338e4c46111210
lisong <song.li@witontek.com>
2016-01-21 17:13:33 +0800

    update

--- lsapp/lsapp-biz-service-impl/src/test/java/ls/demon/app/biz/service/impl/user/UserServiceImplTest.java
+++ lsapp/lsapp-biz-service-impl/src/test/java/ls/demon/app/biz/service/impl/user/UserServiceImplTest.java
@@ -4,6 +4,9 @@
  */
 package ls.demon.app.biz.service.impl.user;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -27,65 +30,82 @@ import org.junit.Test;
  * @version $Id: UserServiceImplTest.java, v 0.1 2014年8月7日 下午2:24:52 lisong Exp $
  */
 public class UserServiceImplTest extends BaseRemoteTest {
+    /**
+    * Logger for this class
+    */
+    private static final Logger logger = LoggerFactory.getLogger(UserServiceImplTest.class);
 
     @Test
     public void testCreateUserRemote() {
-        UserService client = (UserService) context.getBean("userService");
-        User user = new User();
-        user.setUserName("username");
-        user.setPassword("abc");
-        user.setRealName("zhangsan");
-        user.setPid("3111111111111111111");
-        user.setEmail("aa@aa.com");
-        UserResult response = client.createUser(user);
-        logger.info("Response: " + response);
+        try {
+            UserService client = (UserService) context.getBean("userService");
+            User user = new User();
+            user.setUserName("username");
+            user.setPassword("abc");
+            user.setRealName("zhangsan");
+            user.setPid("3111111111111111111");
+            user.setEmail("aa@aa.com");
+            UserResult response = client.createUser(user);
+            logger.info("Response: " + response);
+        } catch (Exception e) {
+            logger.error("", e);
+        }
     }
 
     @Test
     public void testCreateUser2Remote() {
-        UserService client = (UserService) context.getBean("userService2");
-        User user = new User();
-        user.setUserName("username");
-        user.setPassword("abc");
-        user.setRealName("zhangsan");
-        user.setPid("3111111111111111111");
-        user.setEmail("aa@aa.com");
-        UserResult response = client.createUser(user);
-        logger.info("Response: " + response);
+        try {
+            UserService client = (UserService) context.getBean("userService2");
+            User user = new User();
+            user.setUserName("username");
+            user.setPassword("abc");
+            user.setRealName("zhangsan");
+            user.setPid("3111111111111111111");
+            user.setEmail("aa@aa.com");
+            UserResult response = client.createUser(user);
+            logger.info("Response: " + response);
+        } catch (Exception e) {
+            logger.error("", e);
+        }
     }
 
     public static void main(String[] args) {
-        //调用WebService
-        JaxWsProxyFactoryBean factory = new JaxWsProxyFactoryBean();
+        try {
+            //调用WebService
+            JaxWsProxyFactoryBean factory = new JaxWsProxyFactoryBean();
 
-        //        ClientProxy proxy = (ClientProxy) Proxy.getInvocationHandler(chartService);
-        //        Client client = proxy.getClient();
+            //        ClientProxy proxy = (ClientProxy) Proxy.getInvocationHandler(chartService);
+            //        Client client = proxy.getClient();
 
-        Map<String, Object> outProps = new HashMap<String, Object>();
-        outProps.put(WSHandlerConstants.ACTION, WSHandlerConstants.USERNAME_TOKEN);
-        outProps.put(WSHandlerConstants.USER, "admin");
-        outProps.put(WSHandlerConstants.PASSWORD_TYPE, WSConstants.PW_DIGEST);
-        outProps.put(WSHandlerConstants.PW_CALLBACK_CLASS, ClientPasswordCallback.class.getName());
-        //        client.getOutInterceptors().add(new SAAJOutInterceptor());
-        //        client.getOutInterceptors().add(new WSS4JOutInterceptor(outProps));
-        //        WSS4JOutInterceptor wssOut = new WSS4JOutInterceptor(outProps);
-        factory.getOutInterceptors().add(new WSS4JOutInterceptor(outProps));
-        factory.getOutInterceptors().add(new SAAJOutInterceptor());
-        factory.getOutInterceptors().add(new LoggingOutInterceptor());
+            Map<String, Object> outProps = new HashMap<String, Object>();
+            outProps.put(WSHandlerConstants.ACTION, WSHandlerConstants.USERNAME_TOKEN);
+            outProps.put(WSHandlerConstants.USER, "admin");
+            outProps.put(WSHandlerConstants.PASSWORD_TYPE, WSConstants.PW_DIGEST);
+            outProps.put(WSHandlerConstants.PW_CALLBACK_CLASS,
+                ClientPasswordCallback.class.getName());
+            //        client.getOutInterceptors().add(new SAAJOutInterceptor());
+            //        client.getOutInterceptors().add(new WSS4JOutInterceptor(outProps));
+            //        WSS4JOutInterceptor wssOut = new WSS4JOutInterceptor(outProps);
+            factory.getOutInterceptors().add(new WSS4JOutInterceptor(outProps));
+            factory.getOutInterceptors().add(new SAAJOutInterceptor());
+            factory.getOutInterceptors().add(new LoggingOutInterceptor());
 
-        factory.getInInterceptors().add(new LoggingInInterceptor());
+            factory.getInInterceptors().add(new LoggingInInterceptor());
 
-        factory.setAddress("http://localhost:8080/lsapp/services/UserService");
-        factory.setServiceClass(UserService.class);
-        UserService chartService = (UserService) factory.create();
+            factory.setAddress("http://localhost:8080/lsapp/services/UserService");
+            factory.setServiceClass(UserService.class);
+            UserService chartService = (UserService) factory.create();
 
-        User user = new User();
-        user.setUserName("username________");
-        user.setPassword("abc");
-        user.setRealName("zhangsan");
-        user.setPid("3111111111111111111");
-        user.setEmail("aa@aa.com");
-        System.out.println(chartService.createUser(user));
+            User user = new User();
+            user.setUserName("username________");
+            user.setPassword("abc");
+            user.setRealName("zhangsan");
+            user.setPid("3111111111111111111");
+            user.setEmail("aa@aa.com");
+            System.out.println(chartService.createUser(user));
+        } catch (Exception e) {
+            logger.error("", e);
+        }
     }
 
 }
