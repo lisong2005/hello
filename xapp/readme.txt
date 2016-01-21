cfe12cbccca32adc24501f29b1cdb872653da1bf
lisong <song.li@witontek.com>
2016-01-21 15:35:36 +0800

    add

---
+++ xapp/readme.txt
@@ -0,0 +1,6 @@
+http://www.cnblogs.com/quanyongan/archive/2013/05/28/3103243.html
+
+
+http://blog.csdn.net/subuser/article/details/18988813
+
+
---
+++ xapp/xapp-service-facade/src/main/java/org/xapp/service/facade/App.java
@@ -0,0 +1,13 @@
+package org.xapp.service.facade;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
